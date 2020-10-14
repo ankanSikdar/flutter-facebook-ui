3a135d4fcd2a32c5babf579c6c04901704b8de43
Ankan Sikdar <ankansikdar@gmail.com>
2020-10-14 19:01:11 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/flutter_facebook_responsive_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ankansikdar.flutter_facebook_responsive_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/palette.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color scaffold = Color(0xFFF0F2F5);
+
+  static const Color facebookBlue = Color(0xFF1777F2);
+
+  static LinearGradient createRoomGradient = LinearGradient(
+    List: [Color(0xFF496AE1), Color(0xFFCE48B1)],
+  );
+
+  static const Color online = Color(0xFF4BCB1F);
+
+  static LinearGradient storyGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    List: [Colors.transparent, Colors.black26],
+  );
+}
