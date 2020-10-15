5f37ec2fa9968a829e31b68800b37b9853761aac
Ankan Sikdar <ankansikdar@gmail.com>
2020-10-15 20:42:31 +0530

    Added rooms widget and in it create room button

---
+++ lib/widgets/rooms.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  Rooms({this.onlineUsers});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 10.0),
+      color: Colors.white,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+              child: _CreateRoomButton(),
+            );
+          }
+          return Container(
+            margin: const EdgeInsets.all(2.0),
+            height: 20.0,
+            width: 20.0,
+            color: Colors.red,
+          );
+        },
+        itemCount: onlineUsers.length + 1,
+      ),
+    );
+  }
+}
+
+class _CreateRoomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      onPressed: () {},
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      borderSide: BorderSide(
+        color: Colors.blueAccent[100],
+        width: 3.0,
+      ),
+      child: Row(
+        children: [
+          ShaderMask(
+            shaderCallback: (bounds) =>
+                Palette.createRoomGradient.createShader(bounds),
+            child: Icon(
+              Icons.video_call,
+              size: 35.0,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 4.0),
+          Text(
+            'Create\nRoom',
+            style: TextStyle(color: Colors.blueAccent[200]),
+          )
+        ],
+      ),
+    );
+  }
+}
