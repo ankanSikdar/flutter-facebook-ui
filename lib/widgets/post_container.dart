55af2ab86fc0f349a88d9a9b95027a377d28de66
Ankan Sikdar <ankansikdar@gmail.com>
2020-10-16 20:52:43 +0530

    Designed the post header

---
+++ lib/widgets/post_container.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+
+class PostContainer extends StatelessWidget {
+  final Post post;
+
+  PostContainer({this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5.0),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      height: 400,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: _PostHeader(
+              post: post,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  final Post post;
+
+  const _PostHeader({@required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ProfielAvatar(imageUrl: post.user.imageUrl),
+        const SizedBox(width: 8.0),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${post.timeAgo} • ',
+                    style: TextStyle(fontSize: 12.0, color: Colors.grey[600]),
+                  ),
+                  Icon(
+                    Icons.public,
+                    color: Colors.grey[600],
+                    size: 12.0,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        IconButton(icon: Icon(Icons.more_horiz), onPressed: () {}),
+      ],
+    );
+  }
+}
